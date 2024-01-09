1bb39477860b9d1a46fe1878bb10adbb0c9cd916
jericho <jericho_ph@qq.com>
2024-01-09 21:07:26 +0800

    m

--- frontend/src/pages/install/IndoorPlatoonPage.vue
+++ frontend/src/pages/install/IndoorPlatoonPage.vue
@@ -22,7 +22,7 @@
                   <sel-install-indoor-room-type label-name="机房类型" sechma="search" />
                 </div>
                 <div class="col">
-                  <SelInstallIndoorRoom label-name="所属机房" sechma="search" />
+                  <sel-install-indoor-room label-name="所属机房" sechma="search" />
                 </div>
                 <div class="col">
                   <sel-organization-railway_search label-name="所属路局" />
@@ -54,14 +54,125 @@
 
     <q-card class="q-mt-md">
       <q-card-section>
-
+        <div class="row">
+          <div class="col"><span :style="{ fontSize: '20px' }">室内上道位置-排列表</span></div>
+          <div class="col text-right">
+            <q-btn-group>
+              <q-btn color="secondary" label="新建室内上道位置-排" icon="add" @click="fnOpenAlertCreateInstallIndoorPlatoon" />
+              <q-btn color="negative" label="删除室内上道位置-排" icon="deleted" @click="fnDestroyInstallIndoorPlatoons" />
+            </q-btn-group>
+          </div>
+        </div>
+        <div class="row q-mt-md">
+          <div class="col">
+            <q-table flat bordered title="" :rows="rows" row-key="uuid" :pagination="{ rowsPerPage: 200 }"
+              :rows-per-page-options="[50, 100, 200, 0]" rows-per-page-label="分页" :selected-rows-label="() => { }"
+              selection="multiple" v-model:selected="selected">
+              <template v-slot:header="props">
+                <q-tr :props="props">
+                  <q-th align="left"><q-checkbox key="allCheck" v-model="props.selected" /></q-th>
+                  <q-th align="left">#</q-th>
+                  <q-th align="left" key="name" @click="event => fnColumnReverseSort(event, props, sortBy)">
+                    名称
+                  </q-th>
+                  <q-th align="right"></q-th>
+                </q-tr>
+              </template>
+              <template v-slot:body="props">
+                <q-tr :props="props">
+                  <q-td><q-checkbox :key="props.row.uuid" :value="props.row.uuid" v-model="props.selected" /></q-td>
+                  <q-td>{{ props.row.index }}</q-td>
+                  <q-td key="name" :props="props">{{ props.row.name }}</q-td>
+                  <q-td key="operation" :props="props">
+                    <q-btn-group>
+                      <q-btn @click="fnOpenAlertEditInstallIndoorPlatoon(props.row.operation)" color="warning"
+                        icon="edit">
+                        编辑
+                      </q-btn>
+                      <q-btn @click="fnDestroyInstallIndoorPlatoon(props.row.operation)" color="negative" icon="delete">
+                        删除
+                      </q-btn>
+                    </q-btn-group>
+                  </q-td>
+                </q-tr>
+              </template>
+            </q-table>
+          </div>
+        </div>
       </q-card-section>
     </q-card>
   </div>
+
+  <!-- 弹窗 -->
+  <!-- 新建室内上道位置-排弹窗 -->
+  <q-dialog v-model="alertCreateInstallIndoorPlatoon" no-backdrop-dismiss>
+    <q-card :style="{ minWidth: '450px' }">
+      <q-card-section>
+        <div class="text-h6">新建室内上道位置-排</div>
+      </q-card-section>
+      <q-form class="q-gutter-md" @submit.prevent="fnStoreInstallIndoorPlatoon">
+        <q-card-section class="q-pt-none">
+          <div class="row">
+            <div class="col">
+              <q-input outlined clearable lazy-rules v-model="name_alertCreateInstallIndoorPlatoon" label="名称"
+                :rules="[]" />
+            </div>
+          </div>
+          <div class="row q-mt-md">
+            <div class="col">
+              <sel-install-indoor-room-type label-name="机房类型" sechma="alertCreate" />
+            </div>
+          </div>
+          <div class="row q-mt-md">
+            <div class="col">
+              <sel-install-indoor-room label-name="所属机房" sechma="alertCreate" />
+            </div>
+          </div>
+          <div class="row q-mt-md">
+            <div class="col">
+              <sel-organization-railway_search label-name="所属路局" />
+            </div>
+          </div>
+          <div class="row q-mt-md">
+            <div class="col">
+              <sel-organization-paragraph_search label-name="所属站段" />
+            </div>
+          </div>
+          <div class="row q-mt-md">
+            <div class="col">
+              <sel-organization-workshop_search label-name="所属车间" />
+            </div>
+          </div>
+          <div class="row q-mt-md">
+            <div class="col">
+              <sel-organization-station_search label-name="所属站场" />
+            </div>
+          </div>
+          <div class="row q-mt-md">
+            <div class="col">
+              <sel-organization-railway_search label-name="所属中心" />
+            </div>
+          </div>
+          <div class="row q-mt-md">
+            <div class="col">
+              <sel-organization-crossroad_search label-name="所属道口" />
+            </div>
+          </div>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn-group>
+            <q-btn type="button" label="关闭" v-close-popup />
+            <q-btn type="submit" label="确定" icon="check" color="secondary" />
+          </q-btn-group>
+        </q-card-actions>
+      </q-form>
+    </q-card>
+  </q-dialog>
+  <!-- 编辑室内上道位置-排弹窗 -->
 </template>
 <script setup>
 import { ref, onMounted, provide } from "vue";
-import { getVal } from "src/utils/common";
+import { fnColumnReverseSort, getVal } from "src/utils/common";
 import {
   ajaxGetInstallIndoorPlatoons,
   ajaxGetInstallIndoorPlatoon,
@@ -120,6 +231,26 @@ const rows = ref([]);
 const selected = ref([]);
 const sortBy = ref("");
 
+// 新建室内上道位置-排弹窗数据
+const alertCreateInstallIndoorPlatoon = ref("");
+const name_alertCreateInstallIndoorPlatoon = ref("");
+const installIndoorRoomTypeUuid_alertCreateInstallIndoorPlatoon = ref("");
+provide("installIndoorRoomTypeUuid_alertCreate", installIndoorRoomTypeUuid_alertCreateInstallIndoorPlatoon);
+const installIndoorRoomUuid_alertCreateInstallIndoorPlatoon = ref("");
+provide("installIndoorRoomUuid_alertCreate", installIndoorRoomUuid_alertCreateInstallIndoorPlatoon);
+const organizationRailwayUuid_alertCreateInstallIndoorPlatoon = ref("");
+provide("organizationRailwayUuid_alertCreate", organizationRailwayUuid_alertCreateInstallIndoorPlatoon);
+const organizationParagraphUuid_alertCreateInstallIndoorPlatoon = ref("");
+provide("organizationParagraphUuid_alertCreate", organizationParagraphUuid_alertCreateInstallIndoorPlatoon);
+const organizationWorkshopUuid_alertCreateInstallIndoorPlatoon = ref("");
+provide("organizationWorkshopUuid_alertCreate", organizationWorkshopUuid_alertCreateInstallIndoorPlatoon);
+const organizationStationUuid_alertCreateInstallIndoorPlatoon = ref("");
+provide("organizationStationUuid_alertCreate", organizationStationUuid_alertCreateInstallIndoorPlatoon);
+const organizationCenterUuid_alertCreateInstallIndoorPlatoon = ref("");
+provide("organizationCenterUuid_alertCreate", organizationCenterUuid_alertCreateInstallIndoorPlatoon);
+const organizationCrossroadUuid_alertCreateInstallIndoorPlatoon = ref("");
+provide("organizationCrossroadUuid_alertCreate", organizationCrossroadUuid_alertCreateInstallIndoorPlatoon);
+
 onMounted(() => fnInit());
 
 const fnInit = () => fnSearch();
@@ -166,5 +297,13 @@ const fnSearch = () => {
     })
     .catch(e => notifies.error(e.msg));
 };
-
+const fnResetAlertCreateInstallIndoorPlatoon = () => { };
+const fnOpenAlertCreateInstallIndoorPlatoon = () => {
+  alertCreateInstallIndoorPlatoon.value = true;
+ };
+const fnStoreInstallIndoorPlatoon = params => { };
+const fnOpenAlertEditInstallIndoorPlatoon = params => { };
+const fnUpdateInstallIndoorPlatoon = params => { };
+const fnDestroyInstallIndoorPlatoon = params => { };
+const fnDestroyInstallIndoorPlatoons = () => { };
 </script>
