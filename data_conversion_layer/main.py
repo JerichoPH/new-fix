92eb24ed4f66bf0b7fb50ceb692287c6576ce2e1
mac <yujizhou@zces.cn>
2024-01-15 13:45:22 +0800

    m

--- data_conversion_layer/main.py
+++ data_conversion_layer/main.py
@@ -6,6 +6,7 @@ from routers.temp import router as temp_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+aio_rabbit_mq_task: asyncio.Task = None
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,15 +19,18 @@ app.add_middleware(
 
 @app.on_event("startup")
 async def startup_event():
-    pass
-    # aio_rabbit_mq = await AioRabbitMq().conn()
-    # asyncio.create_task(await aio_rabbit_mq.consume_messages())
+    aio_rabbit_mq = await AioRabbitMq().generate_conn()
+    aio_rabbit_mq_task = asyncio.create_task(aio_rabbit_mq.consume_messages())
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    # 清理资源，关闭连接等操作...
     pass
+    # if aio_rabbit_mq_task and not aio_rabbit_mq_task.done():
+    #     aio_rabbit_mq_task.cancel()  # 取消未完成的任务
+    #     await aio_rabbit_mq_task  #
+    # aio_rabbit_mq = await AioRabbitMq().generate_conn()
+    # await aio_rabbit_mq.close_conn()
 
 
 app.include_router(auth_router, prefix="/api", tags=["auth"])
--- data_conversion_layer/routers/temp.py
+++ data_conversion_layer/routers/temp.py
@@ -1,5 +1,5 @@
 from fastapi.routing import APIRouter
-from providers.aioRabbitMq import  AioRabbitMq
+from providers.aioRabbitMq import AioRabbitMq
 from utils.stdBusiness import StdBusniess
 import arrow
 
@@ -12,12 +12,12 @@ router = APIRouter()
 
 @router.get(f"{url_prefix}/sendToRabbitMq")
 async def send_to_rabbit_mq():
-    aio_rabbit_mq = await AioRabbitMq().conn()
-    aio_rabbit_mq = await aio_rabbit_mq.generate_queues()
-    now = arrow.now().format("YYYY-MM-DD HH:mm:ss")
-
+    aio_rabbit_mq = await AioRabbitMq().generate_conn()
     await aio_rabbit_mq.send_message(
-        queue_name="new-fix-backend",
-        message=StdBusniess(business_type="ping", content={"time": now}).to_json_str(),
+        queue_name="backend",
+        message=StdBusniess(
+            business_type="ping",
+            content={"time": arrow.now().format("YYYY-MM-DD HH:mm:ss")},
+        ).to_json_str(),
     )
     return StdResponse().success("发送成功")
